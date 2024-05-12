59666a137bace946f1c9106bf06f1f4c7ff734c4
Duhi95 <127203489+Duhi95@users.noreply.github.com>
2024-05-12 21:27:12 +0200

    Test for Unit Test setup

---
+++ src/test/CharacterTest.java
@@ -0,0 +1,64 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CharacterTest {
+
+	@Test
+	void testGetName() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdjustPump() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDealDamage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResetRemainingSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRemainingSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDecreaseRemainingSteps() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMakeSticky() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWakeUp() {
+		fail("Not yet implemented");
+	}
+
+}
