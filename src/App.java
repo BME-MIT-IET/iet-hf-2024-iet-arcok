877b271c709f303c24b3974e30bfa37ceaefbbae
Duhi95 <127203489+Duhi95@users.noreply.github.com>
2024-05-12 22:48:01 +0200

    Trying to redo2

--- src/App.java
+++
@@ -1,8 +0,0 @@
-/**
- * Az Alkalmazás belépési pontja.
- */
-public class App {
-    public static void main(String[] args) throws Exception {
-       Gui gui = Gui.getInstance();
-    }
-}
--- src/SaboteurPointSource.java
+++
@@ -1,8 +0,0 @@
-/**
- * A szabotőrök számára pontszámot generáló objektumok interfésze.
- */
-public interface SaboteurPointSource {
-    public int measureAndResetLeakedWaterAmount(); /** Visszadja a kifolyott viz mennyiseget, majd nullara allitja */
-
-	public String getName();
-}
