c1796780807cc327081e841e26c9c87f684087b2
rolandandai <87148863+rolandandai@users.noreply.github.com>
2024-05-15 10:01:25 +0200

    Change how password is supplied

--- execute-on-vnc.sh
+++ execute-on-vnc.sh
@@ -3,7 +3,7 @@ NEW_DISPLAY=42
 DONE="no"
 
 mkdir -p ~/.vnc
-echo "password" > ~/.vnc/passwd
+PASSWORD=$(openssl rand -base64 12)
 
 while [ "$DONE" == "no" ]
 do
@@ -23,6 +23,8 @@ echo "Using first available display :${NEW_DISPLAY}"
 OLD_DISPLAY=${DISPLAY}
 vncserver ":${NEW_DISPLAY}" -localhost -geometry 1600x1200 -depth 16
 export DISPLAY=:${NEW_DISPLAY}
+
+echo "$PASSWORD" | x11vnc -storepasswd -input - | sudo tee ~/.vnc/passwd >/dev/null 2>&1
 
 "$@"
 
