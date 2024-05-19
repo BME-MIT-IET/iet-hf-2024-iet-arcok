2ac44455ac3921bfb20036bff74c2b22ab98bba6
abri02 <balint.abraham02@gmail.com>
2024-05-19 22:41:41 +0200

    Fix SonarCloud : Remove of unused commented variables + adding lambda function

--- src/main/java/ActionButton.java
+++ src/main/java/ActionButton.java
@@ -10,7 +10,6 @@ import javax.swing.JButton;
  */
 public class ActionButton extends JButton{
     private ArrayList<Integer> actions;
-    //private Element element;
 
     ActionButton(ArrayList<Integer> acts/*Element _element*/)
     {
@@ -23,66 +22,49 @@ public class ActionButton extends JButton{
 		actions.add(0);
 		actions.add(0);
 
-        //element=_element; 
         /** Ezen keresztul tudjuk meghivni a Control fuggvenyeit.*/
         Control instance = Control.getInstance();
-        
-        addActionListener(
-        new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-				Game.getInstance().getCurrentCharacter().decreaseRemainingSteps();
-                switch(e.getActionCommand()) {
-                case "Move": 
-                	/** Atadja a kivalasztott Elementet amire lepni szeretne.*/
-                	instance.Move(actions.get(0));
-                	break;
-                	/** Valasztott Elementen lyukasztas.*/
-                case "Stab": 
-                	instance.Stab();
-                	break;
-                	/** Valasztott Elementen pumpa elhelyezes.*/
-                case "PlacePump": 
-                	instance.PlacePump();
-                	break;
-                	/** Valasztott Elementen cso elhelyezes.*/
-                case "PlacePipe": 
-                	instance.PlacePipe();
-                	break;
-					/** Valasztott cso vegenek felvetele.*/
-				case "PickupPump": 
-                	instance.PickupPump();
-                	break;
-                	/** Valasztott cso vegenek felvetele.*/
-                case "PickUpPipe": 
-                	instance.PickUpPipe(actions.get(0));
-                	break;
-                	/** Valasztott Element javitasa.*/
-                case "Repair": 
-                	instance.Repair();
-                	break;
-                	/** Valasztott Element ragadossa tetele.*/
-                case "Stick": 
-                	instance.Stick();
-                	break;
-                	/** Valasztott Element csuszossa tetele.*/
-                case "Slime": 
-                	instance.Slime();
-                	break;
-                	/** Pumpa ki es bemenetenek allitasa.*/
-                case "Adjust": 
-                	//ez meg kerdeses
+
+
+		addActionListener(e -> {
+			Game.getInstance().getCurrentCharacter().decreaseRemainingSteps();
+			switch(e.getActionCommand()) {
+				case "Move":
+					instance.Move(actions.get(0));
+					break;
+				case "Stab":
+					instance.Stab();
+					break;
+				case "PlacePump":
+					instance.PlacePump();
+					break;
+				case "PlacePipe":
+					instance.PlacePipe();
+					break;
+				case "PickupPump":
+					instance.PickupPump();
+					break;
+				case "PickUpPipe":
+					instance.PickUpPipe(actions.get(0));
+					break;
+				case "Repair":
+					instance.Repair();
+					break;
+				case "Stick":
+					instance.Stick();
+					break;
+				case "Slime":
+					instance.Slime();
+					break;
+				case "Adjust":
 					instance.Adjust(actions.get(0), actions.get(1));
-                	break;
-                	/** Adott karakter korenek vege.*/
-                case "EndMove": 
-                	instance.EndMove();
-                	break;
+					break;
+				case "EndMove":
+					instance.EndMove();
+					break;
+			}
 
-                }
 
-            }
         });
     }
 }
